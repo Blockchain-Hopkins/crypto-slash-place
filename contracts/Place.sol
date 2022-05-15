81525936a7e8cbfcb67976b088b51fb40aea5a83
David Thomas <davidthomas@davids-mbp.acasmart.jh.edu>
2022-05-15 12:00:04 -0400

    Adding initial fields and constructor

--- contracts/Place.sol
+++ contracts/Place.sol
@@ -1,3 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-// Here we go, boys!
+contract Place {
+    // Contract state variables
+    uint256 width;
+    uint256 height;
+    uint256 num_colors;
+    Pixel[][] grid;
+
+    // A struct to represent a pixel
+    struct Pixel {
+        uint256 color;
+        address payable owner;
+    }
+
+    // Constructor
+    constructor(uint256 _width, uint256 _height, uint256 _num_colors) {
+        width = _width;
+        height = _height;
+        num_colors = _num_colors;
+        Pixel[][] memory pixels = new Pixel[][](width); // This creates 2D array in memory of length width and height any
+        for (uint256 i = 0; i < width; i++) {
+            for (uint256 j = 0; j < height; j++) {
+                grid[i][j] = Pixel(0, payable(0));
+            }
+        }
+        grid = pixels;
+    }
+}
