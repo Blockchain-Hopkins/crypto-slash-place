8e47374eb64b73c1fa635896472ea6852b63a15f
geepara <62688021+geepara@users.noreply.github.com>
2022-05-15 21:41:05 -0400

    landing page bg image attempt

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,32 +1,11 @@
+import bgimg from "../../assets/images/rplace.png";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ position: "fixed", width: "100%", height: "100%", fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <div style={{ position: "fixed", background: `url(${bgimg})`, backgroundRepeat:"no-repeat", backgroundPosition: "50%", backgroundSize: "130%", width: "100%", minHeight:"100%", alignItems: "center", justifyContent: "center" }}>
+        <h1 className="grid-item">Welcome to Remix</h1>
+      </div>
     </div>
   );
 }
