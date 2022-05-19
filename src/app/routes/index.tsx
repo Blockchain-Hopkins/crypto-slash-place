3c534d265e30a55788458fc9924722a3d84d0eb6
Higgs32584 <mjhiggins2@loyola.edu>
2022-05-19 17:35:38 -0400

    stuff

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,9 @@
 import bgimg from "../../assets/images/rplace.png";
-
 export default function Index() {
   return (
-    <div style={{ position: "fixed", width: "100%", height: "100%", fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <div style={{ position: "fixed", background: `url(${bgimg})`, backgroundRepeat:"no-repeat", backgroundPosition: "50%", backgroundSize: "130%", width: "100%", minHeight:"100%", alignItems: "center", justifyContent: "center" }}>
+    <div style={{ position: "fixed", width: "100%", height: "100%", fontFamily: "system-ui, sans-serif", lineHeight: "1.4",color: "black" }}>
         <h1 className="grid-item">Welcome to Remix</h1>
+        <canvas id="tutorial" width="10" height="10" style = {{color: "black",border: "1px solid black"}}></canvas>
       </div>
-    </div>
   );
 }
