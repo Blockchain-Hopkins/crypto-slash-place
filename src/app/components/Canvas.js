c60afcf32ebbb73669fbc1c311effdd73b68390c
Higgs32584 <mjhiggins2@loyola.edu>
2022-05-20 14:05:58 -0400

    added multiple pixels with randomized colors

--- src/app/components/Canvas.js
+++ src/app/components/Canvas.js
@@ -2,13 +2,24 @@ import { useEffect, useRef } from "react";
 
 export default function Canvas(props) {
   const canvasRef = useRef(null);
-
+  function getRndColor() {
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
   useEffect(() => {
-    let canvas = canvasRef.current;
-    let ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(50, 50, 50, 50);
+
+    let canvas = canvasRef.current;
+    canvas.style.position = "relative";
+    canvas.style.right = "0px";
+    canvas.style.top="-3000px";
+    canvas.style.width = "100px";
+    canvas.style.height = "100px";
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = getRndColor();
+    ctx.fillRect(0,0, 300, 300);
   }, []);
 
   return <canvas ref={canvasRef} {...props} />;
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -3,8 +3,13 @@ import { useRef } from "react";
 import Canvas from "../components/Canvas"; 
 
 export default function Index() {
+  var rows = [];
 
-  return (
-    <Canvas />
-  );
+  var numrows = 100;
+  for (var i = 0; i < numrows; i++) {
+      // note: we are adding a key prop here to allow react to uniquely identify each
+      // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+      rows.push(<Canvas key={i} />);
+  }
+  return <tbody>{rows}</tbody>;
 }
