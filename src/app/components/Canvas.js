1aedd52da64fe30d3c6f4221c52b8a778f542762
geepara <62688021+geepara@users.noreply.github.com>
2022-05-21 02:21:42 -0400

    fix pixels, print number when clicked

--- src/app/components/Canvas.js
+++ src/app/components/Canvas.js
@@ -1,26 +1,32 @@
 import { useEffect, useRef } from "react";
 
 export default function Canvas(props) {
+  const { index } = props;
   const canvasRef = useRef(null);
+
   function getRndColor() {
-    var r = 255*Math.random()|0,
-        g = 255*Math.random()|0,
-        b = 255*Math.random()|0;
-    return 'rgb(' + r + ',' + g + ',' + b + ')';
-}
+    var r = (255 * Math.random()) | 0,
+      g = (255 * Math.random()) | 0,
+      b = (255 * Math.random()) | 0;
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+
   useEffect(() => {
-
-
     let canvas = canvasRef.current;
-    canvas.style.position = "relative";
-    canvas.style.right = "0px";
-    canvas.style.top="-3000px";
-    canvas.style.width = "100px";
-    canvas.style.height = "100px";
+    canvas.style.width = "50px";
+    canvas.style.height = "50px";
     let ctx = canvas.getContext("2d");
     ctx.fillStyle = getRndColor();
-    ctx.fillRect(0,0, 300, 300);
+    ctx.fillRect(0, 0, 300, 300);
   }, []);
 
-  return <canvas ref={canvasRef} {...props} />;
+  return (
+    <canvas
+      onClick={() => {
+        console.log(index);
+      }}
+      ref={canvasRef}
+      {...props}
+    />
+  );
 }
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,15 +1,21 @@
 import bgimg from "../../assets/images/rplace.png";
 import { useRef } from "react";
-import Canvas from "../components/Canvas"; 
+import Canvas from "../components/Canvas";
+import "./index.styles.css";
 
 export default function Index() {
   var rows = [];
 
   var numrows = 100;
   for (var i = 0; i < numrows; i++) {
-      // note: we are adding a key prop here to allow react to uniquely identify each
-      // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-      rows.push(<Canvas key={i} />);
+    // note: we are adding a key prop here to allow react to uniquely identify each
+    // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+    rows.push(<Canvas key={i} index={i} />);
   }
-  return <tbody>{rows}</tbody>;
+  return (
+    <>
+      <div className="canvasContent">{...rows}</div>
+      <div className="colorSelection"></div>
+    </>
+  );
 }
